4d05cd3aab8c3d6505106d766edc5499d28685f8
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2017-02-01 14:56:58 -0500

    Add RealSense driver node

--- contrib/environment/connect_me.sh
+++ contrib/environment/connect_me.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 #
-# Automatically try to connect to 'MyWiFi' when WiFi is enabled
+# Automatically try to connect to 'adventure-wifi' when WiFi is enabled
 #
 
 # the output of nmcli should be in English
 LC_ALL=C
 
 # loop for a while until NetworkManager is accepting commands
-# Connection name: New 802-11-wireless connection
-# SSID: copter
+# Connection name: adventure-wifi
+# SSID: <name>-wifi
 # manual IP: 10.42.0.1
 
-nmcli con up id 'New 802-11-wireless connection'
+nmcli con up id 'adventure-wifi'
 while [ "$(nmcli -t -f WIFI,STATE nm)" = 'enabled:disconnected' ]
 do
- nmcli con up id 'turtlebot-wifi'
+ nmcli con up id 'adventure-wifi'
  sleep 5
 done
 
